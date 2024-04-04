99241a20e1479406791f70ba4442bd4abc13b814
MZalfred <cutesy-todo0x@icloud.com>
2024-04-04 06:16:06 +0100

    Create FitShirtsAppApp.swift

---
+++ FitShirtsApp/FitShirtsApp/FitShirtsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitShirtsAppApp.swift
+//  FitShirtsApp
+//
+//  Created by Alfred Kafurama on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitShirtsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
